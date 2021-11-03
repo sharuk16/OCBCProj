8767c6e7eee26864a44557e8cc0773fb4facd5da
S10205010 <73088550+S10205010@users.noreply.github.com>
2021-11-03 20:26:16 +0800

    Add DAL into FundTransferController

--- PFD Challenge 1/Controllers/FundTransferController.cs
+++ PFD Challenge 1/Controllers/FundTransferController.cs
@@ -3,11 +3,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using PFD_Challenge_1.DAL;
 namespace PFD_Challenge_1.Controllers
 {
     public class FundTransferController : Controller
     {
+        BankAccountDAL bankAccountContext = new BankAccountDAL();
+        BankUserDAL bankUserContext = new BankUserDAL();
+        FutureTransferDAL futureTransferContext = new FutureTransferDAL();
+        TransactionDAL transactionDAL = new TransactionDAL();
         public IActionResult FundTransfer()
         {
             return View();
