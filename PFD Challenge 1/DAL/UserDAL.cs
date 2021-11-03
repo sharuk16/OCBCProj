6643547bdc03ffe2d7aadcbeb116d0ebb06a384a
S10205010 <73088550+S10205010@users.noreply.github.com>
2021-11-03 11:42:55 +0800

    Add User DAL

---
+++ PFD Challenge 1/DAL/UserDAL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Data.SqlClient;
+using PFD_Challenge_1;
+using PFD_Challenge_1.Models;
+namespace PFD_Challenge_1.DAL
+{
+    public class UserDAL
+    {
+        private IConfiguration Configuration { get; }
+        private SqlConnection conn;
+        public UserDAL()
+        {
+            //Read ConnectionString from appsettings.json file
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+            string strConn = Configuration.GetConnectionString(
+            "CompetifyConnectionString");
+            //Instantiate a SqlConnection object with the
+            //Connection String read.
+            conn = new SqlConnection(strConn);
+        }
+    }
+}
