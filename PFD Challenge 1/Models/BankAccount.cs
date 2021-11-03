f58d313ddf6f11d9fba93227fc6f50521c3d9338
S10205010 <73088550+S10205010@users.noreply.github.com>
2021-11-03 20:30:39 +0800

    Add Models

---
+++ PFD Challenge 1/Models/BankAccount.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PFD_Challenge_1.Models
+{
+    public class BankAccount
+    {
+    }
+}
