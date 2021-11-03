4203928058b215461eb6bced8567f9a3e320a6a9
S10205010 <73088550+S10205010@users.noreply.github.com>
2021-11-03 20:20:15 +0800

    Add TransactionDAL

---
+++ PFD Challenge 1/DAL/TransactionDAL.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PFD_Challenge_1.DAL
+{
+    public class TransactionDAL
+    {
+        private IConfiguration Configuration { get; }
+        private SqlConnection conn;
+        public TransactionDAL()
+        {
+            //Read ConnectionString from appsettings.json file
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+            string strConn = Configuration.GetConnectionString(
+            "CompetifyConnectionString");
+            //Instantiate a SqlConnection object with the
+            //Connection String read.
+            conn = new SqlConnection(strConn);
+        }
+    }
+}
