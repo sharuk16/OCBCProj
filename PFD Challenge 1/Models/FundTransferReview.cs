681b545855d9a6d71043d062c390fb6e65947959
S10205010 <73088550+S10205010@users.noreply.github.com>
2021-11-10 12:47:32 +0800

    Add FundTransferReview class

---
+++ PFD Challenge 1/Models/FundTransferReview.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PFD_Challenge_1.Models
+{
+    public class FundTransferReview
+    {
+        public string Recipient { get; set; }
+        public decimal Balance { get; set; }
+        public decimal TransferAmount { get; set; }
+        public bool futureTransfer { get; set; }
+        public DateTime? TimeTransfer { get; set; }
+    }
+}
