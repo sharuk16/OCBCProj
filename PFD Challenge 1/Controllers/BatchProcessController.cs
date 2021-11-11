d10f5c7bc4439e3e74278e0015b2c0d9892f891f
Sharuk16 <S10194023@connect.np.edu.sg>
2021-11-11 23:57:23 +0800

    Batch Process

---
+++ PFD Challenge 1/Controllers/BatchProcessController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PFD_Challenge_1.Jobs;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PFD_Challenge_1.Controllers
+{
+    public class BatchProcessController : Controller
+    {
+        
+        }
+    }
+}
--- PFD Challenge 1/Controllers/HomeController.cs
+++ PFD Challenge 1/Controllers/HomeController.cs
@@ -9,22 +9,27 @@ using System.Threading.Tasks;
 using PFD_Challenge_1.DAL;
 using System.Diagnostics;
 
+
 namespace PFD_Challenge_1.Controllers
 {
     public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+    {
+        
+
+       private readonly ILogger<HomeController> _logger;
+
+       public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
-
+    
         public IActionResult Index()
         {
             return View();
         }
 
+      
         public IActionResult Privacy()
         {
             return View();
