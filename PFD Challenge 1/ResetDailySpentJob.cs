d95182b0d0135c248ac270540ce9b03780558aed
Sharuk16 <S10194023@connect.np.edu.sg>
2021-11-18 14:36:57 +0800

    added job to reset daily spent every day

---
+++ PFD Challenge 1/ResetDailySpentJob.cs
@@ -0,0 +1,26 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using PFD_Challenge_1.DAL;
+using PFD_Challenge_1.Models;
+
+namespace PFD_Challenge_1
+{
+    public class ResetDailySpentJob : IJob
+    {
+        TransactionDAL transactionContext = new TransactionDAL();
+        FutureTransferDAL futureTransContext = new FutureTransferDAL();
+        BankAccountDAL bankAccContext = new BankAccountDAL();
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            transactionContext.ResetDailySpend();
+            Console.WriteLine("resetting daily spent");
+            return Task.FromResult(0);
+        }
+    }
+}
