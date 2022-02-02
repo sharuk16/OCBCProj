6c49a13ed7c37b2d3844642d4fb90f4b359b3153
S10205010 <73088550+S10205010@users.noreply.github.com>
2022-02-02 18:11:02 +0800

    Add Credential List to receive response from Passwordless API

---
+++ PFD Challenge 1/Models/CredentialList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PFD_Challenge_1.Models
+{
+    public class Descriptor
+    {
+        public string type { get; set; }
+        public string id { get; set; }
+    }
+
+    public class CredentialList
+    {
+        public string aaGuid { get; set; }
+        public string country { get; set; }
+        public DateTime createdAt { get; set; }
+        public string credType { get; set; }
+        public Descriptor descriptor { get; set; }
+        public string device { get; set; }
+        public DateTime lastUsedAt { get; set; }
+        public string origin { get; set; }
+        public string publicKey { get; set; }
+        public string rpid { get; set; }
+        public int signatureCounter { get; set; }
+        public string userHandle { get; set; }
+        public string userId { get; set; }
+        public object nickname { get; set; }
+    }
+}
