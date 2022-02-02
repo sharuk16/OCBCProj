8a3b4a483ef6fa64fe56ec27c452b7fe1d37f3aa
SomethingFish <73157400+SomethingFish@users.noreply.github.com>
2022-02-02 18:31:53 +0800

    Adds TempTransac.cs

---
+++ PFD Challenge 1/Models/TempTransac.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PFD_Challenge_1.Models
+{
+    public class TempTransac
+    {
+        public string _id { get; set; }
+        public string Checkpoint4 { get; set; }
+        public string Checkpoint3 { get; set; }
+        public string Checkpoint2 { get; set; }
+        public string Nric { get; set; }
+        public string Checkpoint1 { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TimeTransfer { get; set; }
+        public string Recipient { get; set; }
+        public string Sender { get; set; }
+        public int TransacID { get; set; }
+    }
+}
