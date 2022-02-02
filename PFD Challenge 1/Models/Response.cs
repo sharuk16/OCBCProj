a6e0ecbe58d04eadff9d2a649ecff48dde11f150
S10205010 <73088550+S10205010@users.noreply.github.com>
2022-02-02 18:12:20 +0800

    Add Response for receiving passwordless API response for Signin.

---
+++ PFD Challenge 1/Models/Response.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PFD_Challenge_1.Models
+{
+    public class Response
+    {
+        public bool success { get; set; }
+        public string userId { get; set; }
+        public DateTime timestamp { get; set; }
+        public string rpid { get; set; }
+        public string origin { get; set; }
+        public string device { get; set; }
+        public string country { get; set; }
+        public object nickname { get; set; }
+        public DateTime expiresAt { get; set; }
+    }
+}
